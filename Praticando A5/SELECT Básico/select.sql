ce87f3432e552d85276862bdce722a38c16d703e
Wesley Constantino <99278991+WesleyConstantino@users.noreply.github.com>
2022-10-06 10:44:54 -0300

    Update select.sql

--- Praticando A5/SELECT Básico/select.sql
+++ Praticando A5/SELECT Básico/select.sql
@@ -1,5 +1,12 @@
 --Obtenha o nome e o salário dos professores que possuem um salário entre 
 --2500 e 4000 (inclusive estes valores), em ordem decrescente de salário.
 select Nome_Professor, Salario from professor where Salario 
-between 2500 and 4000 order by Salario desc;
+between 2500 and 4000 order by Salario desc; --"desc" determina ordem decrescente
 
+
+--Obtenha o nome e a idade dos alunos cujo nome contenha a letra “i” e que 
+--tenha mais de 19 anos, em ordem crescente do nome do aluno.
+select Nome_Aluno, Idade 
+from Aluno
+where ((Nome_Aluno like 'I%') and (Idade > 19)) -- "<Campo> like '<Caractere>%'" seleciona nomes de um campo que comecem com um determinado caractere
+order by  Nome_Aluno asc; --"order by <Campo> asc" determina ordem crescente
