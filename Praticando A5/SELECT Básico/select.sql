a296900f6d81aa8207650e2f9382cdfdd685e398
Wesley Constantino <99278991+WesleyConstantino@users.noreply.github.com>
2022-10-06 11:03:22 -0300

    Update select.sql

--- Praticando A5/SELECT Básico/select.sql
+++ Praticando A5/SELECT Básico/select.sql
@@ -10,3 +10,10 @@ select Nome_Aluno, Idade
 from Aluno
 where ((Nome_Aluno like 'I%') and (Idade > 19)) -- "<Campo> like '<Caractere>%'" seleciona nomes de um campo que comecem com um determinado caractere
 order by  Nome_Aluno asc; --"order by <Campo> asc" determina ordem crescente
+
+
+-- Obtenha a média de idade e a quantidade de alunos do curso de código igual a 
+--1001.
+select avg(Idade) as Média_Idade, count(Matricula) as Qtde_Alunos --"avg(Campo)" obtém o valor médio de um atributo. "as" <Nome_Qualquer> determina nome da tabela de exibição resultante
+from Aluno                      --"COUNT" obtém o número de linhas analisadas
+where (Cod_curso = 1001);
