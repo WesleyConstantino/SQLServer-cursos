92e6795834d08c8c9f963f8847102e165e8307ae
Wesley Constantino <99278991+WesleyConstantino@users.noreply.github.com>
2022-10-06 09:58:13 -0300

    Create select.sql

---
+++ Praticando A5/SELECT Básico/select.sql
@@ -0,0 +1,5 @@
+--Obtenha o nome e o salário dos professores que possuem um salário entre 
+2500 e 4000 (inclusive estes valores), em ordem decrescente de salário.
+
+select Nome_Professor, Salario from professor where Salario 
+between 2500 and 4000 order by Salario desc;
