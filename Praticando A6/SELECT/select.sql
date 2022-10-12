846f98e9d4327f167426059f029fbef39955c319
Wesley Constantino <99278991+WesleyConstantino@users.noreply.github.com>
2022-10-12 13:17:22 -0300

    Update select.sql

--- Praticando A6/SELECT/select.sql
+++ Praticando A6/SELECT/select.sql
@@ -1,6 +1,7 @@
 --Consultas realizadas no Oracle Live SQL (https://livesql.oracle.com/) 
 
 --Questão 1
+--Obtenha o nome de cada aluno e o nome do curso a que cada um pertence.
  SELECT A.Nome_Aluno, C.Nome_Curso
  FROM Aluno A, Curso C
  WHERE(A.Cod_Curso = C.Cod_Curso);
