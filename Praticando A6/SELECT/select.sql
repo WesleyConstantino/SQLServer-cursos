0de93a5212c89ee60bf14dfb9951bfeebfda33ce
Wesley Constantino <99278991+WesleyConstantino@users.noreply.github.com>
2022-10-12 13:18:01 -0300

    Update select.sql

--- Praticando A6/SELECT/select.sql
+++ Praticando A6/SELECT/select.sql
@@ -5,3 +5,7 @@
  SELECT A.Nome_Aluno, C.Nome_Curso
  FROM Aluno A, Curso C
  WHERE(A.Cod_Curso = C.Cod_Curso);
+
+
+--Questão 2
+--Obtenha a maior idade, a menor idade e a idade média dos alunos do curso de 'Ciencia da Computacao'.
